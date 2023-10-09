2fa95734a320c4042519db3122f7785f3f36207e
Alexey Sokolov <alexvyber@gmail.com>
2023-10-09 19:24:03 +0300

    init + portals

---
+++ one/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Portal } from "./components/portal";
+
+function App() {
+	return (
+		<>
+			<Portal />
+		</>
+	);
+}
+
+export default App;
---
+++ one/src/components/portal.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+
+export const Portal = () => {
+	const [isOpen, setOpen] = useState(false);
+	return (
+		<>
+			<button onClick={() => setOpen(true)}>Open Portal</button>
+			<Alert isOpen={isOpen} onClose={() => setOpen(false)}>
+				Some Text
+			</Alert>
+		</>
+	);
+};
+
+function Alert({
+	onClose,
+	isOpen,
+	children,
+}: React.PropsWithChildren & { onClose: () => void; isOpen: boolean }) {
+	if (!isOpen) return null;
+
+	return createPortal(
+		<div
+			onClick={onClose}
+			style={{
+				background: "rgba(0,0,0,.25)",
+				position: "absolute",
+				top: 0,
+				left: 0,
+				width: "100%",
+				height: "100%",
+				display: "flex",
+				justifyContent: "center",
+				placeItems: "center",
+			}}
+		>
+			<div
+				onClick={(e) => e.stopPropagation()}
+				style={{ background: "white", padding: "20px" }}
+			>
+				{children}
+			</div>
+		</div>,
+		document.getElementById("alert")!,
+	);
+}
